004e5796661911839a9800411010798f6d2d9f3c
hapax <admin@ichormedia.com.au>
2015-10-15 05:34:01 -0400

    Added CSRF token support to request calls.

--- core/request.js
+++ core/request.js
@@ -7,13 +7,40 @@
 import Rest from './class/rest';
 
 var request = (function() {
-  var vm = {};
+  var vm = {
+    lock: m.prop(false)
+  };
 
   vm.request = function(request, options) {
     var params = {
       data: request,
       settings: options ? options : {}
     };
+
+    // check for CSRF token; otherwise request one.
+    if (!vm.lock() && !mdrupal.user.token()) {
+      vm.lock(true)
+
+      new Rest({
+        data: {
+          method: 'GET',
+          url: '/rest/session/token',
+          extract: function(xhr) {
+            var token = xhr.responseText;
+  
+            if (xhr.status == 200) {
+              token = JSON.stringify(xhr.responseText);
+            }
+
+            // Set CSRF token.
+            return mdrupal.user.token(token);
+          }
+        },
+        settings: {
+          format: ''
+        }
+      });
+    }
 
     return new Rest(params).response;
   }
