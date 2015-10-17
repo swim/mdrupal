3a930fc0ffe9f1d30113e34ae971a49266c57ce6
hapax <admin@ichormedia.com.au>
2015-10-17 02:30:03 -0400

    Provide error handling on request response.

--- core/class/rest.js
+++ core/class/rest.js
@@ -5,7 +5,7 @@
  * Format request params for Drupal REST to parse.
  *
  * @todo, allow option to set auth type used.
- * @todo, check to ensure config is not already set.
+ * @todo, improve request error handling.
  */
 import Request from './request';
 
@@ -16,7 +16,10 @@ class Rest extends Request {
     this.params.url += '?_format=' + format;
 
     // Set request headers.
-    this.requestHeaders();    
+    this.requestHeaders();
+
+    // Handle malformed response.
+    this.requestExtract();
 
     // Process request.
     super.process();
@@ -24,14 +27,34 @@ class Rest extends Request {
 
   requestHeaders() {
     var headers = this.settings.headers ? this.settings.headers : [];
-    this.params.config = function(xhr) {
-      for (var i = 0; i < headers.length; i++) {
-        xhr.setRequestHeader(headers[i].type, headers[i].value);
-      }
 
-      // Set CSRF token, if available.
-      if (md.user.token()) {
-        xhr.setRequestHeader('X-CSRF-Token', md.user.token());
+    if (!this.params.config) {
+      this.params.config = function(xhr) {
+        for (var i = 0; i < headers.length; i++) {
+          xhr.setRequestHeader(headers[i].type, headers[i].value);
+        }
+
+        // Set CSRF token, if available.
+        if (md.user.token()) {
+          xhr.setRequestHeader('X-CSRF-Token', md.user.token());
+        }
+      }
+    }
+  }
+
+  requestExtract() {
+    if (!this.params.extract) {
+      this.params.extract = function(xhr, xhrOptions) {
+        try {
+          if (xhr.status == 404) {
+            throw JSON.parse(xhr.response); 
+          }
+  
+          return xhr.response;
+        }
+        catch (e) {
+          throw new Error(e.message)
+        }
       }
     }
   }
